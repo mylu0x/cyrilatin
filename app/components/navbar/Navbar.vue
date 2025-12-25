f6872ada9889ee9a55643882fbda32acd4c3b4ef
mylu0x <mylu0x@gmail.com>
2025-12-25 16:36:50 +0900

    feat(navbar): add navbar

---
+++ app/components/navbar/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="py-14px flex flex-col items-end gap-4px">
+    <NuxtLink to="/" class="flex items-center w-full gap-4px px-12px py-3px bg-white b-(solid 1px gray-2) shadow-sm hover:bg-gray-50 transition-all-50 rounded-8px">
+      <Icon name="tabler:home" class="text-gray-8" />
+      <span class="text-gray-8 font-500 text-14px">Home</span>
+    </NuxtLink>
+<!--    <div class="flex flex-col items-center w-full">-->
+<!--      <p class="text-12px font-500 text-gray-8 flex items-center justify-center gap-4px"><Icon name="circle-flags:ru" /> Russian</p>-->
+<!--    </div>-->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <div class="flex w-screen h-screen bg-gray-50 justify-center py-256px">
+  <div class="flex w-screen h-screen bg-gray-50 justify-center py-256px gap-8px">
+    <div>
+      <Navbar />
+    </div>
     <div class="w-500px h-300px bg-white rounded-16px b-(solid 1px gray-2) p-12px shadow-sm outline-(solid 1px gray-1 3px)">
       <slot />
     </div>
