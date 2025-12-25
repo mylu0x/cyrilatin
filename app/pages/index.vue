910c5d4a6255323faf38562d5333ed941146bf0f
mylu0x <mylu0x@gmail.com>
2025-12-25 17:02:50 +0900

    feat(home): add welcome message

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-  <p>Hello, World!</p>
+  <div class="flex items-center justify-center w-full h-full">
+    <div class="text-center">
+      <p class="text-32px font-600 text-gray-8 leading-tight">Welcome!</p>
+      <p class="text-gray-7 text-14px"><strong>Cyrilatin</strong> is a tool that converts characters into Latin letters.</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+strong { @apply text-gray-8 font-600 }
 </style>
