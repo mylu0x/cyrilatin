ac56eed4c9378b52acc5112703e93942e3d76740
mylu0x <mylu0x@gmail.com>
2025-12-25 17:08:36 +0900

    style(navbar): increase width of home link

--- app/components/navbar/Navbar.vue
+++ app/components/navbar/Navbar.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="py-14px flex flex-col items-end gap-4px">
     <div class="bg-gray-1 p-4px rounded-8px">
-      <NuxtLink to="/" class="flex items-center w-full gap-4px px-12px py-3px bg-white b-(solid 1px gray-2) shadow-sm hover:bg-gray-50 transition-all-50 rounded-8px">
+      <NuxtLink to="/" class="flex items-center w-full gap-4px px-18px py-3px bg-white b-(solid 1px gray-2) shadow-sm hover:bg-gray-50 transition-all-50 rounded-8px">
         <Icon name="tabler:home" class="text-gray-8" />
         <span class="text-gray-8 font-500 text-14px">Home</span>
       </NuxtLink>
