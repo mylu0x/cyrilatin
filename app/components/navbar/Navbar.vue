ce231483033fd8a86e16bec7558e6b9289c563f8
mylu0x <mylu0x@gmail.com>
2025-12-25 18:35:40 +0900

    feat(navbar): add converter link

--- app/components/navbar/Navbar.vue
+++ app/components/navbar/Navbar.vue
@@ -4,15 +4,18 @@
 
 <template>
   <div class="py-14px flex flex-col items-end gap-4px">
-    <div class="bg-gray-1 p-4px rounded-8px">
+    <NavbarSection>
       <NuxtLink to="/" class="flex items-center w-full gap-4px px-18px py-3px bg-white b-(solid 1px gray-2) shadow-sm hover:bg-gray-50 transition-all-50 rounded-8px">
         <Icon name="tabler:home" class="text-gray-8" />
         <span class="text-gray-8 font-500 text-14px">Home</span>
       </NuxtLink>
-    </div>
-<!--    <div class="flex flex-col items-center w-full">-->
-<!--      <p class="text-12px font-500 text-gray-8 flex items-center justify-center gap-4px"><Icon name="circle-flags:ru" /> Russian</p>-->
-<!--    </div>-->
+    </NavbarSection>
+    <NavbarSection>
+      <p class="text-12px font-500 text-gray-8 flex items-center justify-center gap-4px"><Icon name="circle-flags:ru" /> Russian</p>
+      <div class="text-14px w-full mt-2px">
+        <NuxtLink to="/ru/iso9" class="flex items-center justify-center rounded-8px font-500 py-3px w-full hover:bg-gray-2 transition-all-50">ISO-9</NuxtLink>
+      </div>
+    </NavbarSection>
   </div>
 </template>
 
---
+++ app/components/navbar/NavbarSection.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-gray-1 p-4px rounded-8px w-full">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
