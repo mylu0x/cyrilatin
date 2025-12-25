bc53104b19e6d2169536a2754738725ae7a000fa
mylu0x <mylu0x@gmail.com>
2025-12-25 15:42:54 +0900

    style(layout): add style for main content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <div class="flex w-screen h-screen bg-gray-1 justify-center py-256px">
-    <slot />
+  <div class="flex w-screen h-screen bg-gray-50 justify-center py-256px">
+    <div class="w-500px h-300px bg-white rounded-16px b-(solid 1px gray-2) p-12px shadow-sm outline-(solid 1px gray-1 3px)">
+      <slot />
+    </div>
   </div>
 </template>
 
