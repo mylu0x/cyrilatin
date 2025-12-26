94af5811e6145fbbfd88dc1b9be7e2141c1f3bfe
mylu0x <mylu0x@gmail.com>
2025-12-26 12:08:36 +0900

    refactor: componentize navbar links

---
+++ app/components/navbar/NavbarTranslitLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+  to?: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to class="flex items-center justify-center rounded-8px font-500 py-3px w-full hover:bg-gray-2 transition-all-50">
+    {{ text }}
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
