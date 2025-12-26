7e21cc68962157493890f7aa16bd7c8d6f1e1ec2
mylu0x <mylu0x@gmail.com>
2025-12-26 12:17:05 +0900

    style(NavbarTranslitLink): add background color for active link

--- app/components/navbar/NavbarTranslitLink.vue
+++ app/components/navbar/NavbarTranslitLink.vue
@@ -3,10 +3,14 @@ const props = defineProps<{
   text: string;
   to?: string;
 }>();
+const route = useRoute();
+const isLinkActive = computed(() => {
+  return route.path === props.to;
+});
 </script>
 
 <template>
-  <NuxtLink :to class="flex items-center justify-center rounded-8px font-500 py-3px w-full hover:bg-gray-2 transition-all-50">
+  <NuxtLink :to :class="{ 'bg-gray-2' : isLinkActive }" class="flex items-center justify-center rounded-8px font-500 py-3px w-full hover:bg-gray-2 transition-all-50">
     {{ text }}
   </NuxtLink>
 </template>
