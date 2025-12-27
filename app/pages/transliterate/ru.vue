a399f8393173d9e8d4a24f235133891ccfbc6772
mylu0x <mylu0x@gmail.com>
2025-12-27 19:53:30 +0900

    feat(transliterate): add pretty rule selector

--- app/pages/transliterate/ru.vue
+++ app/pages/transliterate/ru.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import useTransliterateRu from "~/composables/useTransliterateRu";
+import { OnClickOutside } from '@vueuse/components';
 
-const transliterationType = ref<'iso9'>('iso9');
+type Rule = {
+  id: 'iso9';
+  name: string;
+  desc: string;
+}
+
+const currentRule = ref<Rule>({
+  id: 'iso9',
+  name: 'ISO 9',
+  desc: 'WHATWHAT',
+});
+
+const rules = ref<Rule[]>([
+  {
+    id: 'iso9',
+    name: 'ISO 9',
+    desc: 'An international standard for transliterating Cyrillic letters into Latin letters'
+  }
+]);
+
+const isSelectorOpen = ref<boolean>(true);
 
 const originalText = ref<string>('');
 const resultText = computed(() => {
-  return useTransliterateRu(transliterationType.value, originalText.value);
+  return useTransliterateRu(currentRule.value.id, originalText.value);
 });
 
 const copyOriginalText = () => {
@@ -43,9 +64,24 @@ const clearResultText = () => {
         </button>
       </div>
       <div class="flex w-full px-4px">
-        <select v-model="transliterationType" class="w-full py-3px px-6px bg-white b-(solid 1px gray-2) shadow-sm rounded-8px w-full text-gray-8 focus:b-gray-3 focus:outline-none">
-          <option value="iso9">ISO 9</option>
-        </select>
+        <div class="relative w-full">
+          <OnClickOutside @trigger="isSelectorOpen = false">
+            <button @click="isSelectorOpen = !isSelectorOpen" class="flex items-center px-6px py-3px bg-white b-(solid 1px gray-2) w-full shadow-sm rounded-8px hover:bg-gray-1 transition-all-100 focus:outline-none focus:b-(solid 1px gray-3)">
+              {{ currentRule.name }}
+            </button>
+            <div v-show="isSelectorOpen" class="absolute left-1/2 -translate-x-1/2">
+              <div class="flex flex-col gap-4px bg-white z-999 b-(solid 1px gray-2) shadow-sm w-fit p-6px rounded-12px items-center mt-4px shadow-md">
+                <div v-for="rule in rules" @click="currentRule = rule; isSelectorOpen = false;" class="cursor-pointer transition-all-50 flex gap-32px hover:bg-gray-1 px-12px py-6px rounded-10px w-300px items-center justify-between">
+                  <div class="leading-tight">
+                    <p class="whitespace-nowrap text-16px font-600 text-gray-8">{{ rule.name }}</p>
+                    <p class="text-12px text-gray-7">{{ rule.id }}</p>
+                  </div>
+                  <p class="text-12px text-gray-8">{{ rule.desc }}</p>
+                </div>
+              </div>
+            </div>
+          </OnClickOutside>
+        </div>
       </div>
       <div class="shrink-0 p-4px bg-gray-1 rounded-b-12px b-(solid 1px gray-2px) !b-t-none flex gap-4px mb-6px">
         <button @click="copyResultText" class="flex items-center bg-white px-8px py-2px gap-3px rounded-8px b-(solid 1px gray-2) shadow-sm text-gray-9 hover:bg-gray-1 transition-all-100">
