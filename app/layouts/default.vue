2585d6b54c082d82ecff3d9afa90cc3785a3f57c
mylu0x <mylu0x@gmail.com>
2025-12-28 10:45:08 +0900

    style(layout): add style for mobile (classes)

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <div class="layout flex w-screen h-screen bg-gray-50 justify-center pt-200px gap-8px">
-    <div>
+  <div class="layout flex w-screen h-screen bg-gray-50 justify-center pt-200px gap-8px max-md:flex-col">
+    <div class="max-md:mx-16px">
       <Navbar />
     </div>
     <div>
-      <div class="w-500px bg-white rounded-16px b-(solid 1px gray-2) p-12px shadow-sm outline-(solid 1px gray-1 3px)">
+      <div class="w-500px bg-white rounded-16px b-(solid 1px gray-2) p-12px shadow-sm outline-(solid 1px gray-1 3px) max-md:(w-auto mx-16px)">
         <slot />
       </div>
     </div>
