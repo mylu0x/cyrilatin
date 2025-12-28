a4753b960655692fc6725d555a518c3f136906de
mylu0x <mylu0x@gmail.com>
2025-12-28 10:40:54 +0900

    feat(home): add updates section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
+type Update = {
+  date: string;
+  content: string;
+}
 
+const updates: Update[] = [
+  {
+    date: 'December 28, 2025',
+    content: 'Added updates section to the home page',
+  }
+]
 </script>
 
 <template>
-  <div class="flex items-center justify-center w-full h-200px">
-    <div class="text-center">
+  <div class="flex flex-col items-center justify-center w-full gap-16px">
+    <div class="text-center my-32px">
       <p class="text-32px font-600 text-gray-8 leading-tight">Welcome!</p>
       <p class="text-gray-7 text-14px"><strong>Cyrilatin</strong> is a tool that converts characters into Latin letters.</p>
+    </div>
+    <div class="flex flex-col w-full px-24px b-t-(solid 1px gray-2) py-16px">
+      <div v-for="update in updates" class="flex items-center justify-between w-full gap-4px">
+        <p class="font-600 text-gray-8 text-14px">{{ update.date }}</p>
+        <p class="text-14px text-gray-7">{{ update.content }}</p>
+      </div>
     </div>
   </div>
 </template>
