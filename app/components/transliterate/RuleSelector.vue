2f3c068d3973ff3994b301a5a619aea4b574a8d5
mylu0x <mylu0x@gmail.com>
2025-12-29 18:49:12 +0900

    style(RuleSelector): adjust width of rule title

--- app/components/transliterate/RuleSelector.vue
+++ app/components/transliterate/RuleSelector.vue
@@ -20,7 +20,7 @@ const isSelectorOpen = ref<boolean>(false);
       <div v-show="isSelectorOpen" class="absolute left-1/2 -translate-x-1/2">
         <div class="flex flex-col gap-4px bg-white z-999 b-(solid 1px gray-2) shadow-sm w-fit p-6px rounded-12px items-center mt-4px shadow-md">
           <div v-for="rule in rules" :key="rule.id" @click="model = rule; isSelectorOpen = false;" class="select-none cursor-pointer transition-all-50 flex hover:bg-gray-1 px-12px py-6px rounded-10px w-300px items-center justify-between">
-            <div class="leading-tight w-100px flex flex-col shrink-0">
+            <div class="leading-tight w-120px flex flex-col shrink-0">
               <p class="whitespace-nowrap text-16px font-600 text-gray-8">{{ rule.name }}</p>
               <p class="text-12px text-gray-7">{{ rule.id }}</p>
             </div>
