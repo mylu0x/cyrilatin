2c6148f576ef76ccacfc9cdb0ebbb8c3d38666bd
mylu0x <mylu0x@gmail.com>
2025-12-29 18:09:11 +0900

    feat: add error page

---
+++ app/error.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+const props = defineProps<{ error: NuxtError }>();
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="flex flex-col items-center text-center leading-tight py-24px">
+      <h1 class="text-48px text-red-6 font-700">{{ error.statusCode }}</h1>
+      <p class="text-gray-8">{{ error.statusMessage }}</p>
+      <NuxtLink to="/" class="my-6px text-blue-6 hover:underline">Go back home</NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
