d6a48cc0c894d207e9e639da667f7d016fd9b476
mylu0x <mylu0x@gmail.com>
2026-01-01 00:09:40 +0900

    chore(footer): HAPPY NEW YEAR

--- app/components/navbar/Footer.vue
+++ app/components/navbar/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <footer class="flex flex-col py-24px text-center text-14px text-gray-7">
-    <p>&copy; 2025 mylu0x - Released under the MIT License.</p>
+    <p>&copy; 2026 mylu0x - Released under the MIT License.</p>
     <p><a href="https://github.com/HatScripts/circle-flags" target="_blank" rel="noopener noreferrer" class="text-blue-6">Circle Flags</a> &copy; 2024 HatScripts</p>
   </footer>
 </template>
